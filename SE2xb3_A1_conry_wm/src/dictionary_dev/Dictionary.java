78f1297a3b97f818f5a3902d105fd426eee30e3d
menavya16 <sachdn4@mcmaster.ca>
2017-03-08 13:18:15 -0700

    Comments are added to all the constructors

--- SE2xb3_A1_conry_wm/src/dictionary_dev/Dictionary.java
+++ SE2xb3_A1_conry_wm/src/dictionary_dev/Dictionary.java
@@ -14,38 +14,54 @@ package dictionary_dev;
 import java.util.*;			// For ArrayList
 public class Dictionary {
     
-    private ArrayList<Integer> keys = new ArrayList<Integer>(20);
+    /**
+     * Two arraylists are created for keys and values with a fixed size, providing extra methods 
+     *  functionalities.
+     */
+	private ArrayList<Integer> keys = new ArrayList<Integer>(20);
     private ArrayList<String> values = new ArrayList<String>(20);
 
+    /**
+     * this constructor is empty, and is quiet redundant here, should be removed 
+     */
     public Dictionary() {}
     
+    /**
+     * Initializes the contructor and instantiates the lists using valueOf method
+     * that is part of arraylist library which takes in the value input by the user
+     * and returns the string representation which is then parsed into an integer because
+     * the arraylist is of integer type. Value is added into the values (arraylist) 
+     * @param key
+     * @param value
+     */
     public Dictionary(int key, String value) {
         this.keys.add(Integer.valueOf(key));
         this.values.add(value);    
     }
     
     /**
-     * The (key,value) pairs cannot be more than 20, and for every key inserted there 
-     * should be a value. There can be various values for a given key but a key cannot 
-     * have the same value. If these conditions are met then the (key,value) pair will
-     * be added to it. 
-     * 
+     *  Initialization happens in array form for both keys and values
+     *  
      * @param keys
      * @param values
      */
     public Dictionary(int[] keys, String[] values) {
-        if(keys.length > 20 | values.length > 20) {
-            System.out.println("Error maximum entries for dictionary is 20");
-        } if(keys.length != values.length) {
-            System.out.println("Error each key must have a corresponding value");
-        } else {       
-            for(int i = 0; i < keys.length; i++) {                          
-               if(this.keys.contains(Integer.valueOf(keys[i]))) {
-                   System.out.println("Error duplicate keys aren't allowed");
+        if(keys.length > 20 | values.length > 20) {//If the length of either keys or values exceeds 20 
+            System.out.println("Error maximum entries for dictionary is 20");//display the error message, because
+            //the lists cannot exceed more than 20, since the size was specified in the beginning of the class
+        } if(keys.length != values.length) { // If the length of the keys exceeds values or vice versa
+            System.out.println("Error each key must have a corresponding value");//display the error message
+            	// since it has to be a (key,value) pair. So, for every key there must be a corresponding value
+        } else { //otherwise      
+            for(int i = 0; i < keys.length; i++) {// for every i... over the keys arraylist                          
+               if(this.keys.contains(Integer.valueOf(keys[i]))) { // checks to see if all the keys are unique
+            	   // as multiple keys can have same values but vice versa is not allowed
+                   System.out.println("Error duplicate keys aren't allowed");// hence it displays error message
+                   // if multiple values have same keys 
                    System.out.println("Duplicate key and corresponding value ommited");
-               } else {
-                   this.keys.add(Integer.valueOf(keys[i]));
-                   this.values.add(values[i]);
+               } else {//otherwise
+                   this.keys.add(Integer.valueOf(keys[i]));// add the keys
+                   this.values.add(values[i]);// and corresponding values to the arraylists keys,values
                }
            }
         }
